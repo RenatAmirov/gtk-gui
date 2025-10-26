01970cc5b38f4a72a29c696c431766be46328bd5
amirov renat <amirov.renat.1st@gmail.com>
2025-10-26 20:51:49 +0400

    Misc changes

--- app/Main.hs
+++ app/Main.hs
@@ -1,50 +1,122 @@
-{- buttonBox: Build a window with three buttons, the third one, not following others
-     when you grow the window.
- -}
+{- actionMenu: Show you how to make a small File and a small Edit menu (also a button bar)
+  You will learn how to attach actions you want to do when they are activated. -}
 
-module Main (main) where
-
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE OverloadedStrings #-}
 import Graphics.UI.Gtk
+import Data.Text
 
-main :: IO ()
+-- A function like this can be used to tag string literals for i18n.
+-- It also avoids a lot of type annotations.
+__ :: Text -> Text
+__ = id -- Replace with getText from the hgettext package in localised versions
+
+uiDef =
+  "<ui>\
+  \  <menubar>\
+  \    <menu name=\"File\" action=\"FileAction\">\
+  \      <menuitem name=\"New\" action=\"NewAction\" />\
+  \      <menuitem name=\"Open\" action=\"OpenAction\" />\
+  \      <menuitem name=\"Save\" action=\"SaveAction\" />\
+  \      <menuitem name=\"Body\" action=\"BodyAction\" />\
+  \      <menuitem name=\"SaveAs\" action=\"SaveAsAction\" />\
+  \      <separator/>\
+  \      <menuitem name=\"Exit\" action=\"ExitAction\"/>\
+  \      <placeholder name=\"FileMenuAdditions\" />\
+  \    </menu>\
+  \    <menu name=\"Edit\" action=\"EditAction\">\
+  \      <menuitem name=\"Cut\" action=\"CutAction\"/>\
+  \      <menuitem name=\"Copy\" action=\"CopyAction\"/>\
+  \      <menuitem name=\"Paste\" action=\"PasteAction\"/>\
+  \    </menu>\
+  \  </menubar>\
+  \  <toolbar>\
+  \    <placeholder name=\"FileToolItems\">\
+  \      <separator/>\
+  \      <toolitem name=\"New\" action=\"NewAction\"/>\
+  \      <toolitem name=\"Open\" action=\"OpenAction\"/>\
+  \      <toolitem name=\"Save\" action=\"SaveAction\"/>\
+  \      <toolitem name=\"Body\" action=\"BodyAction\"/>\
+  \      <separator/>\
+  \    </placeholder>\
+  \    <placeholder name=\"EditToolItems\">\
+  \      <separator/>\
+  \      <toolitem name=\"Cut\" action=\"CutAction\"/>\
+  \      <toolitem name=\"Copy\" action=\"CopyAction\"/>\
+  \      <toolitem name=\"Paste\" action=\"PasteAction\"/>\
+  \      <separator/>\
+  \    </placeholder>\
+  \  </toolbar>\
+  \</ui>" :: Text
+
 main = do
   initGUI
 
-  -- Create a new window
-  window <- windowNew
+  -- Create the menus
+  fileAct <- actionNew "FileAction" (__"File") Nothing Nothing
+  editAct <- actionNew "EditAction" (__"Edit") Nothing Nothing
 
-  -- Here we connect the "destroy" event to a signal handler.
-  on window objectDestroy mainQuit
+  -- Create menu items
+  newAct <- actionNew "NewAction" (__"New")
+            (Just (__"Clear the spreadsheet area."))
+            (Just stockNew)
+  on newAct actionActivated $ putStrLn "New activated."
+  openAct <- actionNew "OpenAction" (__"Open")
+            (Just (__"Open an existing spreadsheet."))
+            (Just stockOpen)
+  on openAct actionActivated $ putStrLn "Open activated."
+  saveAct <- actionNew "SaveAction" (__"Save")
+            (Just (__"Save the current spreadsheet."))
+            (Just stockSave)
+  on saveAct actionActivated $ putStrLn "Save activated."
+  bodyAct <- actionNew "BodyAction" (__"Body")
+            (Just (__"Надо подкачаться!!!"))
+            (Just stockSave)
+  on bodyAct actionActivated $ putStrLn "Body activated."
+  saveAsAct <- actionNew "SaveAsAction" (__"SaveAs")
+            (Just (__"Save spreadsheet under new name."))
+            (Just stockSaveAs)
+  on saveAsAct actionActivated $ putStrLn "SaveAs activated."
+  exitAct <- actionNew "ExitAction" (__"Exit")
+            (Just (__"Exit this application."))
+            (Just stockSaveAs)
+  on exitAct actionActivated $ mainQuit
+  cutAct <- actionNew "CutAction" (__"Cut")
+            (Just (__"Cut out the current selection."))
+            (Just stockCut)
+  on cutAct actionActivated $ putStrLn "Cut activated."
+  copyAct <- actionNew "CopyAction" (__"Copy")
+            (Just (__"Copy the current selection."))
+            (Just stockCopy)
+  on copyAct actionActivated $ putStrLn "Copy activated."
+  pasteAct <- actionNew "PasteAction" (__"Paste")
+            (Just (__"Paste the current selection."))
+            (Just stockPaste)
+  on pasteAct actionActivated $ putStrLn "Paste activated."
 
-  -- Sets the border width of the window.
-  set window [ containerBorderWidth := 10 ]
+  standardGroup <- actionGroupNew ("standard"::Text)
+  mapM_ (actionGroupAddAction standardGroup) [fileAct, editAct]
+  mapM_ (\act -> actionGroupAddActionWithAccel standardGroup act (Nothing::Maybe Text))
+    [newAct, openAct, saveAct, bodyAct, saveAsAct, exitAct, cutAct, copyAct, pasteAct]
 
-  hbuttonbox <- hButtonBoxNew
+  ui <- uiManagerNew
+  mid <- uiManagerAddUiFromString ui uiDef
+  uiManagerInsertActionGroup ui standardGroup 0
 
-  set window [ containerChild := hbuttonbox ]
+  win <- windowNew
+  on win objectDestroy mainQuit
+  on win sizeRequest $ return (Requisition 200 100)
+  (Just menuBar) <- uiManagerGetWidget ui ("/ui/menubar"::Text)
+  (Just toolBar) <- uiManagerGetWidget ui ("/ui/toolbar"::Text)
 
-  button1 <- buttonNewWithLabel "One"
-  button2 <- buttonNewWithLabel "Two"
-  button3 <- buttonNewWithLabel "Three"
+  edit <- textViewNew
+  vBox <- vBoxNew False 0
+  set vBox [boxHomogeneous := False]
+  boxPackStart vBox menuBar PackNatural 0
+  boxPackStart vBox toolBar PackNatural 0
+  boxPackStart vBox edit PackGrow 0
 
-  -- Add each button to the button box with the default packing and padding
-  set hbuttonbox [ containerChild := button
-                 | button <- [button1, button2, button3] ]
+  containerAdd win vBox
+  widgetShowAll win
 
-  -- This sets button3 to be a so called 'secondary child'. When the layout
-  -- style is ButtonboxStart or ButtonboxEnd, the secondary children are
-  -- grouped separately from the others. Resize the window to see the effect.
-  --
-  -- This is not interesting in itself but shows how to set child attributes.
-  -- Note that the child attribute 'buttonBoxChildSecondary' takes the
-  -- button box container child 'button3' as a parameter.
-  set hbuttonbox [ buttonBoxLayoutStyle := ButtonboxStart
-                 , buttonBoxChildSecondary button3 := False ]
-
-  -- The final step is to display everything (the window and all the widgets
-  -- contained within it)
-  widgetShowAll window
-
-  -- All Gtk+ applications must run the main event loop. Control ends here and
-  -- waits for an event to occur (like a key press or mouse event).
   mainGUI
