fda14238d52184523861af812f9d358080b1a0bb
amirov renat <amirov.renat.1st@gmail.com>
2025-11-10 20:53:24 +0400

    Add RnxDns. - Commit 2.

--- app/Main.hs
+++ app/Main.hs
@@ -10,6 +10,7 @@ import Data.Text (Text, pack, unpack)
 import System.Process (readProcess)
 import Network.Socket (getAddrInfo, defaultHints, addrAddress, addrFlags, AddrInfo(..))
 import Network.Socket (AddrInfoFlag(AI_CANONNAME), SocketType(Stream))
+import Control.Exception (catch, SomeException)  -- Added import
 
 -- Упрощенное разрешение имени хоста
 resolveHostname :: String -> IO (Either String [String])
@@ -40,14 +41,12 @@ getLocalIPs = do
         else return $ take 1 allResults  -- Возвращаем первый успешный результат
   where
     tryCommand cmd args = do
-        result <- catchAny (readProcess cmd args "") (\_ -> return "")
-        return $ if null result then "" else cmd ++ ": " ++ take 100 result  -- Ограничиваем длину
+        result <- catch (readProcess cmd args "") (\_ -> return "")
+        return $ if null result then "" else cmd ++ ": " ++ take 100 result
 
-        
--- Универсальный обработчик исключений
+-- Универсальный обработчик исключений (simplified version)
 catchAny :: IO a -> (SomeException -> IO a) -> IO a
-catchAny = Control.Exception.catch
-
+catchAny = catch
 
 -- Основной интерфейс
 createWindow :: IO ()
@@ -164,8 +163,8 @@ createWindow = do
     _ <- Gtk.statusbarPush statusbar statusContext (pack "Получение локальных IP-адресов...")
     
     localIPs <- getLocalIPs
-    let resultText = (pack "Локальные IP-адреса:\n" ++ unlines (map ("  - " ++) localIPs))
-    _ <- updateResults resultText
+    let resultText = "Локальные IP-адреса:\n" ++ unlines (map ("  - " ++) localIPs)
+    updateResults resultText
     Gtk.statusbarPop statusbar statusContext
     Gtk.statusbarPush statusbar statusContext (pack "Локальные IP-адреса получены")
   
@@ -179,7 +178,7 @@ createWindow = do
   _ <- Gtk.on window Gtk.objectDestroy Gtk.mainQuit
   
   -- Инициализация
-  _ <- updateResults (pack "Добро пожаловать в DNS Resolver!\n\n" ++
+  _ <- updateResults ("Добро пожаловать в DNS Resolver!\n\n" ++
                 "Используйте:\n" ++
                 "- 'Разрешить имя' для поиска IP по имени хоста\n" ++
                 "- 'Мои IP-адреса' для просмотра локальных адресов\n" ++
